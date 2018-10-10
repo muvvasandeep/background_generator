59268738ecb15deca92873a3a2f11ae381b24ac2
Victor Cerutti <victor.cerutti@gmail.com>
2018-10-10 11:05:29 +0200

    Display hex codes in <h2>
    https://github.com/manolingam/background_generator/issues/1

--- index_css_js.js
+++ index_css_js.js
@@ -4,9 +4,11 @@ var body = document.getElementById("body");
 
 color1.addEventListener("input",function(){
 	body.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
+	document.querySelector("#h2").innerHTML = color1.value+" "+color2.value
 })
 
 color2.addEventListener("input",function(){
 	body.style.background = "linear-gradient(to right,"+color1.value+","+color2.value+")";
+	document.querySelector("#h2").innerHTML = color1.value+" "+color2.value
 })
 
